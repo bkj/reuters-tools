5d9d3d057db18bd26333e556788a5549311cc6d4
bkj <ben@gophronesis.com>
2016-09-14 12:15:08 -0400

    source credentials

--- get-reuters.sh
+++ get-reuters.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source credentials
 wget -r --user $RUSER --password $RPASS --no-check-certificate https://ir.nist.gov/reuters/
 find ir.nist.gov | grep -E "xz|gz" | xargs -I {} cp {} ./
 rm -r ir.nistgov
